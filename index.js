72eed8a9ce6492afa5f8ed44ae492888a06d4ac2
zinabu kifle <zinabukifle0972@gmail.com>
2025-12-08 01:32:59 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-         
-        
-         
-   
-         document.querySelectorAll('a[href^="#"]').forEach((anchor)=>{
-           anchor.addEventListener('click', function(e){
-                e.preventDefault();
-                   document.querySelector(this.getAttribute("href")).scrollIntoView({behavior: "smooth",
-    });  
-    });  
-     });
-     document.getElementById("contact-form")
-     .addEventListener("submit", function(e){
-     e.preventDefault();
-     this.reset();
+     function Welcome(){
+    alert("Welcome to my portfolio!");
+}
+
+function sendMessage(){
+    alert("Your message has been sent successfully! Thank You");
+}    
+      
+   document.querySelectorAll('a[href^="#"]').forEach((anchor)=>{
+ anchor.addEventListener("onclick", function(e){
+  e.preventDefault();
+ document.querySelector(this.getAttribute("href")).scrollIntoView({behavior: "smooth",
+ });  
+ });  
+ }); 
+ document.getElementById("contact-form")
+ .addEventListener("submit", function(e){
+ e.preventDefault();
+  this.reset();
      
     });
   
